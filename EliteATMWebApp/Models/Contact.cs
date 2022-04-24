0627300dc18130d8a9d1ecba05bc0f2c8fdc9b13
Svengalist <elliotmorrison58@gmail.com>
2022-04-24 14:01:04 -0500

    Added Contact Class to Models

---
+++ EliteATMWebApp/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace EliteATMWebApp.Models
+{
+    public class Contact
+    {
+        public string? Email { get; set; }
+        public string? Subject { get; set; }
+        public string? Comments { get; set; }
+
+    }
+}
