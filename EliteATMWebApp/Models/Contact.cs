7a2081c00fa3bf67b9706e77baf77cc8601c160f
Svengalist <elliotmorrison58@gmail.com>
2022-04-24 21:23:13 -0500

    Added 2 new fields to Contact.cs

--- EliteATMWebApp/Models/Contact.cs
+++ EliteATMWebApp/Models/Contact.cs
@@ -5,6 +5,11 @@
         public string? Email { get; set; }
         public string? Subject { get; set; }
         public string? Comments { get; set; }
-
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string getFullName()
+        {
+            return FirstName + " " + LastName;
+        }
     }
 }
